2a320a5817a4a243a178bd2289503954ff1b449a
MariAndrade <marielama@outlook.com.br>
2021-02-16 19:36:29 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.cyan[50],
+        accentColor: Colors.blueGrey[300],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueGrey[300],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
