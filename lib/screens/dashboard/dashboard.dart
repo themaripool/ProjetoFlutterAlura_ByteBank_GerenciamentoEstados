58909984b49fd8540b4b299a3fc630cb79172a59
MariAndrade <marielama@outlook.com.br>
2021-02-18 16:57:09 -0300

    [ADD] Dashboard page

---
+++ lib/screens/dashboard/dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:bytebank/models/saldo.dart';
+import 'package:bytebank/screens/dashboard/saldo.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Bytebank"),
+        ),
+        body: Align(
+          alignment: Alignment.topCenter,
+          child: SaldoCard(Saldo(10.00)),
+        ));
+  }
+}
